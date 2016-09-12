5a52f0b4557fef229750ca54f41819d5c9f09a6c
Sherif Soliman <sherif@ssoliman.com>
2016-09-12 07:01:22 -0400

    Add bitbar metadata.

--- punread.10m.sh
+++ punread.10m.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# <bitbar.title>punread</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Sherif Soliman</bitbar.author>
+# <bitbar.author.github>sheriferson</bitbar.author.github>
+# <bitbar.desc>Show pinboard unread count</bitbar.desc>
+# <bitbar.dependencies>python</bitbar.dependencies>
+# <bitbar.abouturl>https://github.com/sheriferson/punread</bitbar.abouturl>
+
 count=$(/usr/local/bin/python3 /Users/sherif/projects/punread/punread.py)
 icon="📖"
 echo "$icon $count"
