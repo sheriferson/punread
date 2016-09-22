c260408318572254ca20f8fdc0ee8f9a06c5fbfa
Sherif Soliman <sherif@ssoliman.com>
2016-09-21 20:37:12 -0400

    Add some logic to track unread and total bookmark counts. It might go away later.

--- punread.py
+++ punread.py
@@ -2,6 +2,7 @@ import json
 import os.path
 import pickle
 import requests
+import time
 
 # get the path to punread.py
 pathToMe = os.path.realpath(__file__)
@@ -11,11 +12,23 @@ last_updated_path = os.path.join(pathToMe, 'lastupdated.timestamp')
 unread_count_path = os.path.join(pathToMe, 'unread.count')
 api_token_path = os.path.join(pathToMe, 'api_token')
 
+backup_file = '/Users/sherif/persanalytics/data/unread_pinboard_counts.csv'
+
+def log_counts(total_count, unread_count):
+   """
+   A function to write the time, total bookmark count, and unread bookmark count
+   to a csv file.
+   """
+   now = int(time.time()) 
+   row = str(now) + ',' + str(total_count) + ',' + str(unread_count) + '\n'
+
+   with open(backup_file, 'a') as bfile:
+       bfile.write(row)
+
 with open(api_token_path, 'rb') as f:
     pintoken = f.read().strip()
 
 par = {'auth_token': pintoken, 'format': 'json'}
-
 
 if os.path.isfile(last_updated_path) and os.path.isfile(unread_count_path):
     last_updated = pickle.load(open(last_updated_path, 'rb'))
@@ -36,9 +49,13 @@ if last_updated != last_updated_api:
     links = json.loads(r.text)
 
     unread = [link for link in links if (link['toread'] == 'yes')]
+    total_count = len(links)
+    unread_count = len(unread)
 
     pickle.dump(last_updated_api, open(last_updated_path, 'wb'))
-    pickle.dump(len(unread), open(unread_count_path, 'wb'))
-    print(len(unread))
+    pickle.dump(unread_count, open(unread_count_path, 'wb'))
+
+    log_counts(total_count, unread_count)
+    print(unread_count)
 else:
     print(unread_count)
