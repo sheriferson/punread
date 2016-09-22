a44c344cb3eb9f69d82a61113b30c41a75e27aa4
Sherif Soliman <sherif@ssoliman.com>
2016-09-22 18:13:33 -0400

    Big fix in shell script.

--- punread.10m.sh
+++ punread.10m.sh
@@ -9,7 +9,7 @@
 
 count=$(/usr/local/bin/python3 /Users/sherif/projects/punread/punread.py)
 icon="📖"
-echo -e $icon ${count}
+echo "$icon $count"
 
 echo "---"
 echo "📌 Random article | href=https://pinboard.in/random/?type=unread"
